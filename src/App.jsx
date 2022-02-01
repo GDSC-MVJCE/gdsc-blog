ce649af2242126a156536d88d8214998cba69709
Shruti Pulstya <shrutipulstya9@gmail.com>
2022-02-01 19:33:52 +0530

    added navbar and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DashboardPage from "./pages/dashboard/dashboard.pages";
+import DetailsPage from "./pages/detailspage/details.pages";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div >
+        <Routes>
+          <Route path='/' element={<DashboardPage />} />
+          <Route path='/details-page' element={<DetailsPage />}/>
+        </Routes>
     </div>
   );
 }
--- src/pages/dashboard/dashboard.pages.jsx
+++ src/pages/dashboard/dashboard.pages.jsx
@@ -1,8 +1,53 @@
-import React from "react";
+import React, {Component} from "react";
 import "./dashboard.styles.css";
+import { Button, Container } from 'react-bootstrap';
+import Navbar from 'react-bootstrap/Navbar';
 
 function DashboardPage() {
-  return <div></div>;
+  return (
+    <div>
+      <body>
+        <>
+          <Navbar bg="white" variant="light" >
+            <Container>
+            <div className="d-flex flex-row">
+              <div>
+                <img 
+                  alt=""
+                  src="./logo.svg"
+                  width="65"
+                  height="65"
+                  position = 'absolute'
+                  className="d-inline-block align-top"
+                />
+              </div>
+              <div className="d-flex flex-column">
+                <h5 className="p-2 col-example text-left" >Google Developer Students Club</h5>
+                <h7 className="p-2 col-example text-left">MVJ College of Engineering</h7>
+              </div>
+            </div>
+              
+              <Navbar.Collapse className="justify-content-end">
+                  <img id="round"
+                      alt=""
+                      src="./profile-image.jpeg"
+                      width="30"
+                      height="30"
+                      border-radius = '40'
+                      position = 'absolute'
+                      className="d-inline-block align-top"
+                    />
+                    <Navbar.Text>
+                      <a href="#">Shebin Joseph</a>
+                    </Navbar.Text>
+                  </Navbar.Collapse>
+            </Container>
+          </Navbar> 
+        </>
+
+      </body>
+    </div>
+  );
 }
 
 export default DashboardPage;
